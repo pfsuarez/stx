4a8500741a974e15f5212c0e164f5fc8338c5477
pfsuarez <pfsuarez>
2017-01-13 11:44:48 -0300

    initial commit

---
+++ stx-projects/stx-projects/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="stx_projects.MvcApplication" Language="C#" %>
---
+++ stx-projects/stx-projects/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(stx_projects.Startup))]
+namespace stx_projects
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
